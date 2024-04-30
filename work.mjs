b8971ca52d8be76f20d9df7377a26947bfdb37fe
yhx-12243 <yhx12243@gmail.com>
2024-04-29 23:43:44 -0400

    chore: update worker

--- work.mjs
+++ work.mjs
@@ -1,21 +1,19 @@
 #!/usr/bin/env node
 
 import { execFile } from 'child_process';
+import { readFileSync } from 'fs';
 import { request as httpRequest } from 'http';
 import { Agent as httpsAgent, get as httpsGet, request as httpsRequest } from 'https';
 import { promisify } from 'util';
 
-const execInner = promisify(execFile);
-
 import puppeteer from 'puppeteer';
 
-import Headers from './headers.json' with { type: 'json' };
-
-const sleep = ms => new Promise(f => setTimeout(f, ms));
+const
+	execInner = promisify(execFile),
+	sleep = ms => new Promise(f => setTimeout(f, ms));
 
 const UAs = [
 	'Mozilla/5.0 (Linux; Android 8.1.0; Moto G (4)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36 PTST/240201.144844',
-	'Mozilla/5.0 (iPhone; CPU iPhone OS 15_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/124.0.6261.62 Mobile/15E148 Safari/604.1',
 	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Config/92.2.2788.20',
 	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 Agency/98.8.8175.80',
 	'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
@@ -101,7 +99,7 @@ function requestWithProxy(config) {
 		'-x', `http://${config.proxy.host}:${config.proxy.port}`,
 		'-H', `Cookie: ${config.headers.Cookie}`,
 		'-A', config.headers['User-Agent'],
-	]).then(({ stdout }) => stdout);
+	], { maxBuffer: 1 << 24 }).then(({ stdout }) => stdout);
 }
 
 function fetchWork() {
@@ -132,71 +130,79 @@ function submit(id, content) {
 	return promise;
 }
 
-const port = Number(process.argv[2]);
-if (!Number.isSafeInteger(port)) {
-	async function worker(port, headers) {
-		const nsp = `\x1b[1;35m[${port}]\x1b[0m `;
-		for (; ; ) {
-			const works = await fetchWork();
-			if (!works.length) break;
-			for (const work of works) {
-				const url = `https://www.blackhatworld.com/seo/${work}`;
-				console.log(nsp + '\x1b[33mscraping\x1b[0m %o ...', url);
+async function worker(port, headers) {
+	const nsp = `\x1b[1;35m[${port}]\x1b[0m `;
+	for (; ; ) {
+		const works = await fetchWork();
+		if (!works.length) break;
+		for (const work of works) {
+			const url = `https://www.blackhatworld.com/seo/${work}`;
+			console.log(nsp + '\x1b[33mscraping\x1b[0m %o ...', url);
 
-				const content = await requestWithProxy({
-					headers,
-					url: `https://www.blackhatworld.com/seo/${work}`,
-					proxy: {
-						host: 'dc.smartproxy.com',
-						port,
-					},
-				});
+			const content = await requestWithProxy({
+				headers,
+				url: `https://www.blackhatworld.com/seo/${work}`,
+				proxy: {
+					host: 'dc.smartproxy.com',
+					port,
+				},
+			});
 
-				if (!content.match(/<title>.*BlackHatWorld<\/title>/)) {
-					console.log(nsp + '\x1b[31mwrong\x1b[0m %o: %s', url, content);
-					await sleep(4000);
-					continue;
-				}
-
-				const [, body2] = await submit(work, content);
-				if (body2.toString() !== '""') {
-					console.log(nsp + '\x1b[31merror\x1b[0m %o: %s', url, body2.toString());
-				} else {
-					console.log(nsp + '\x1b[36mfinished\x1b[0m %o ...', url);
-				}
-				await sleep(2800 + Math.random() * 600);
+			if (!content.match(/<title>.*BlackHatWorld<\/title>/ms)) {
+				console.log(nsp + '\x1b[31mwrong\x1b[0m %o: %s', url, content);
+				await sleep(4000);
+				continue;
 			}
+
+			const [, body2] = await submit(work, content);
+			if (body2.toString() !== '""') {
+				console.log(nsp + '\x1b[31merror\x1b[0m %o: %s', url, body2.toString());
+			} else {
+				console.log(nsp + '\x1b[36mfinished\x1b[0m %o ...', url);
+			}
+			await sleep(2400 + Math.random() * 600);
 		}
 	}
-
-	const workers = [];
-	for (const port_ in Headers) {
-		const port = Number(port_);
-		if (!Number.isSafeInteger(port)) continue;
-		workers.push(worker(port, Headers[port_]));
-	}
-
-	await Promise.allSettled(workers);
-	process.exit(0);
 }
 
-const ua = UAs[Math.floor(Math.random() * UAs.length)];
-console.log('choosing user-agent %o ...', ua);
+const port = Number(process.argv[2]);
+if (Number.isSafeInteger(port)) {
 
-const browser = await puppeteer.launch({
-	headless: false,
-	args: [`--proxy-server=http://dc.smartproxy.com:${port}`, '--disable-http2'],
-});
+	const ua = UAs[Math.floor(Math.random() * UAs.length)];
+	console.log('choosing user-agent %o ...', ua);
 
-const [page] = await browser.pages();
-await page.setUserAgent(ua);
-await page.authenticate(credential);
-await page.goto('https://www.blackhatworld.com/');
-await page.waitForSelector('div.p-body', { timeout: 0 });
+	const browser = await puppeteer.launch({
+		headless: false,
+		args: [`--proxy-server=http://dc.smartproxy.com:${port}`, '--disable-http2'],
+	});
 
-const cookies = await page.cookies();
-console.log('cookies:', cookies);
-const cookiesDict = cookies.map(cookie => [cookie.name, cookie.value]);
-const cookieStr = cookiesDict.map(([name, value]) => `${name}=${value}`).join('; ');
+	const [page] = await browser.pages();
+	await page.setUserAgent(ua);
+	await page.authenticate(credential);
+	await page.goto('https://www.blackhatworld.com/');
+	await page.waitForSelector('div.p-body', { timeout: 0 });
 
-await new Promise(() => {});
+	const cookies = await page.cookies();
+	console.log('cookies:', cookies);
+	const cookiesDict = cookies.map(cookie => [cookie.name, cookie.value]);
+	const cookieStr = cookiesDict.map(([name, value]) => `${name}=${value}`).join('; ');
+
+	await new Promise(() => {});
+
+} else {
+	let Headers;
+	try {
+		Headers = JSON.parse(readFileSync(process.argv[2]));
+	} catch {}
+
+	if (Headers) {
+		const workers = [];
+		for (const port_ in Headers) {
+			const port = Number(port_);
+			if (!Number.isSafeInteger(port)) continue;
+			workers.push(worker(port, Headers[port_]));
+		}
+
+		await Promise.allSettled(workers);
+	}
+}
